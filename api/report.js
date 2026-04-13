e25ecc58ca7ac1f2efb161cfa21d6e101391f291
Nagaon <habibaslam12369@gmail.com>
2026-04-13 22:51:24 +0530

    Update report.js

--- api/report.js
+++ api/report.js
@@ -1,14 +1,16 @@
 export default async function handler(req, res) {
 
   const REPORT_URL = process.env.REPORT_URL;
+  const API_KEY = process.env.API_KEY;
 
   try {
-    const response = await fetch(REPORT_URL);
-    const text = await response.text();
 
-    res.status(200).send(text);
+    const response = await fetch(`${REPORT_URL}?key=${API_KEY}`);
+    const data = await response.text();
+
+    return res.status(200).send(data);
 
   } catch (err) {
-    res.status(500).json({ error: "Report fetch failed" });
+    return res.status(500).json({ error: "Report fetch failed" });
   }
 }
