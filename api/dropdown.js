3be35e787409cf77023388b291f005a1c5d8fc4a
Nagaon <habibaslam12369@gmail.com>
2026-04-14 22:23:42 +0530

    Create dropdown.js

---
+++ api/dropdown.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  try {
+    const url = process.env.DROPDOWN_API_URL;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch dropdown data" });
+  }
+}
